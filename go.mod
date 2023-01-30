af660b33edf7ad62588c6cc88394335eab0b69be
zacharygilliom <zacharygilliom@gmail.com>
2023-01-29 22:50:17 -0500

    Added ability to continually search through photos.

--- go.mod
+++ go.mod
@@ -4,9 +4,17 @@ go 1.19
 
 require (
 	cloud.google.com/go/compute/metadata v0.2.0 // indirect
+	github.com/gadelkareem/cachita v0.2.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gphotosuploader/google-photos-api-client-go/v2 v2.4.0 // indirect
+	github.com/gphotosuploader/googlemirror v0.5.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
+	github.com/hashicorp/go-retryablehttp v0.6.8 // indirect
+	github.com/mediocregopher/radix/v3 v3.2.0 // indirect
+	github.com/vmihailenco/msgpack v4.0.1+incompatible // indirect
 	golang.org/x/net v0.5.0 // indirect
 	golang.org/x/oauth2 v0.4.0 // indirect
+	google.golang.org/api v0.19.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 )
--- internal/gmail/gmail.go
+++ internal/gmail/gmail.go
@@ -1,32 +1,109 @@
 package gmail
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
+	"net/http"
+	"os"
 
+	"github.com/gphotosuploader/googlemirror/api/photoslibrary/v1"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 )
 
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	defer f.Close()
+	if err != nil {
+		return nil, err
+	}
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+func getClient(config *oauth2.Config) *http.Client {
+	tokFile := "config/token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	defer f.Close()
+	if err != nil {
+		log.Fatalf("Unable to cache OAuth token: %v", err)
+	}
+	json.NewEncoder(f).Encode(token)
+}
+
+// Requests a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code: %v", err)
+	}
+
+	tok, err := config.Exchange(oauth2.NoContext, authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web: %v", err)
+	}
+	return tok
+}
+
+func getMediaItems(p *photoslibrary.Service, searchFilters photoslibrary.SearchMediaItemsRequest) string {
+	var nextPageToken string
+	mItems := p.MediaItems
+	searchParams := mItems.Search(&searchFilters)
+	result, err := searchParams.Do()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, v := range result.MediaItems {
+		fmt.Println(v.ProductUrl)
+	}
+	nextPageToken = result.NextPageToken
+	return nextPageToken
+
+}
+
 //VerifyGmail will start our api connection to google photos
 func VerifyGmail() {
 
-	content, err := ioutil.ReadFile("../../config/credentials.json")
+	configFile, err := os.ReadFile("config/credentials.json")
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	//TODO: Actually figure out how to do authorization.  Something is wrong here and not allowing the authorization to go through
-	conf, err := google.ConfigFromJSON(content)
-	url := conf.AuthCodeURL("state")
-	fmt.Printf("Visit the URL for the auth dialog: %v", url)
-
-	tok, err := conf.Exchange(oauth2.NoContext, "authorization-code")
+	config, err := google.ConfigFromJSON(configFile, "https://www.googleapis.com/auth/photoslibrary.readonly")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Unable to parse credentials.json file")
 	}
-	client := conf.Client(oauth2.NoContext, tok)
-	client.Get("...")
+
+	client := getClient(config)
+
+	photosService, err := photoslibrary.New(client)
+
+	var nextPageToken string
+	searchFilters := photoslibrary.SearchMediaItemsRequest{PageSize: 50}
+	nextPageToken = getMediaItems(photosService, searchFilters)
+
+	//While more photos are found, they will be continually printed.
+	for nextPageToken != "" {
+		searchFilters.PageToken = nextPageToken
+		getMediaItems(photosService, searchFilters)
+	}
 
 }
